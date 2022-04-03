540cee7f0f400ddc17bf001214542846ccb7d2c0
Jiaying Wei <jiaying@bitgym.com>
2022-04-03 10:59:07 -0700

    Moving tailwind.config.js into _includes

--- _includes/tailwind.config.js
+++ _includes/tailwind.config.js
@@ -1,12 +1,25 @@
 module.exports = {
-  future: {
-    // removeDeprecatedGapUtilities: true,
-    // purgeLayersByDefault: true,
-  },
-  purge: [],
-  theme: {
-    extend: {},
-  },
-  variants: {},
-  plugins: [],
-}
+    purge: [
+      './**/*.html',
+      './assets/js/*.js'
+    ],
+    darkMode: false,
+    theme: {
+      fontFamily: {
+        'sans': ['Jost','ui-sans-serif', 'system-ui'],
+      },
+      extend:{
+        colors: {
+          transparent: 'transparent',
+          current: 'currentColor',
+            },
+        },
+    },
+    variants: {
+      extend:{
+      },
+    },
+    plugins: [
+      require('@tailwindcss/typography'),
+    ],
+  }
